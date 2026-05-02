6fbc74473f0bcc766197614c249128db0c5d80d6
eylulnaz607 <eylulnaz607@users.noreply.github.com>
2026-05-02 13:41:58 +0000

    deploy: 9d7ce69bb58284ce61fe1460c05ab9067eed804d

--- _next/static/_v57HY3t8Ca6hTQd_nAwN/_buildManifest.js
+++ _next/static/_v57HY3t8Ca6hTQd_nAwN/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/portfolyo__//_next/:path+",
+        "destination": "/portfolyo__/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
